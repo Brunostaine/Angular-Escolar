913bfe417d918174440324864ce2691c704f0193
Bruno Staine <brunostaine@hotmail.com>
2022-05-20 14:08:33 -0300

    refatoração componentes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { RegimentoEscolarComponent } from './views/regimento-escolar/regimento-escolar.component';
+import { CalendarioEscolarComponent } from './views/calendario-escolar/calendario-escolar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -5,6 +7,8 @@ import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'calendario-escolar', component: CalendarioEscolarComponent },
+  { path: 'regimento-escolar', component: RegimentoEscolarComponent },
   
 
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,29 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BotaoAdicionarComponent } from './components/botoes/botao-adicionar/botao-adicionar.component';
-import { BotaoCancelarComponent } from './components/botoes/botao-cancelar/botao-cancelar.component';
-import { IconeDeleteComponent } from './components/icones/icone-delete/icone-delete.component';
-import { IconeEditComponent } from './components/icones/icone-edit/icone-edit.component';
 import { CardDiarioTitleComponent } from './components/cards/card-diario-title/card-diario-title.component';
-import { CardTitlePagesComponent } from './components/cards/card-title-pages/card-title-pages.component';
-import {
-  MatCartRavRelatoriosComponent,
-} from './components/cards/mat-cart-rav-relatorios/mat-cart-rav-relatorios.component';
 import { FooterComponent } from './components/templates/footer/footer.component';
 import { HeaderComponent } from './components/templates/header/header.component';
 import { NavComponent } from './components/templates/nav/nav.component';
 import { AngularMaterialModule } from './shared/angular-material/angular-material.module';
-import { CalendarioEscolarComponent } from './views/calendario-escolar/calendario-escolar.component';
 import { HomeComponent } from './views/home/home.component';
-import { CreateAlunosComponent } from './views/rav-relatorios/alunos/create-alunos/create-alunos.component';
-import { ListAlunosComponent } from './views/rav-relatorios/alunos/list-alunos/list-alunos.component';
-import { UpdateAlunosComponent } from './views/rav-relatorios/alunos/update-alunos/update-alunos.component';
-import { RavRelatoriosComponent } from './views/rav-relatorios/rav-relatorios.component';
-import { RavRelatoriosModule } from './views/rav-relatorios/rav-relatorios.module';
-import { CreateRelatoriosComponent } from './views/rav-relatorios/relatorios/create-relatorios/create-relatorios.component';
-import { ListRelatoriosComponent } from './views/rav-relatorios/relatorios/list-relatorios/list-relatorios.component';
-import { RegimentoEscolarComponent } from './views/regimento-escolar/regimento-escolar.component';
 import { SecretariaModule } from './views/secretaria/secretaria.module';
 
 
@@ -37,27 +20,19 @@ import { SecretariaModule } from './views/secretaria/secretaria.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     NavComponent,
     HomeComponent,
-    RavRelatoriosComponent,
-    CreateAlunosComponent,
-    ListAlunosComponent,
-    MatCartRavRelatoriosComponent,
-    CardTitlePagesComponent,
-    UpdateAlunosComponent,
+    FooterComponent,
+    
+    
+    
 
     CardDiarioTitleComponent,
-    CalendarioEscolarComponent,
-    RegimentoEscolarComponent,
+    
+    
 
     
-    ListRelatoriosComponent,
-          CreateRelatoriosComponent,
-          BotaoAdicionarComponent,
-          IconeEditComponent,
-          IconeDeleteComponent,
-          BotaoCancelarComponent,
+    
           
     
   ],
@@ -69,7 +44,7 @@ import { SecretariaModule } from './views/secretaria/secretaria.module';
     SecretariaModule,
     
     AngularMaterialModule,
-    RavRelatoriosModule,
+    
     
 
   ],
