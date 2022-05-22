4862a49d75daec9ab9ab7f2423f99433ceafe66d
Bruno Staine <brunostaine@hotmail.com>
2022-05-22 19:40:02 -0300

    title page alunos

--- frontend/src/app/views/secretaria/alunos/alunos.component.ts
+++ frontend/src/app/views/secretaria/alunos/alunos.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunosComponent implements OnInit {
 
+  title: string = 'Alunos'
   alunos: string = '/alunos'
 
   constructor() { }
