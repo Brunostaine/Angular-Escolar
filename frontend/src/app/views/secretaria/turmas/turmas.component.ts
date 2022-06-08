81e3a9290b78a7121f8e95b43024cf0658437901
Bruno Staine <brunostaine@hotmail.com>
2022-06-08 14:13:46 -0300

    create - save turmas

--- frontend/src/app/views/secretaria/turmas/turmas.component.ts
+++ frontend/src/app/views/secretaria/turmas/turmas.component.ts
@@ -1,3 +1,6 @@
+import { Turma } from './../../../models/turma-model';
+import { Router } from '@angular/router';
+import { TurmasService } from './../../../services/turmas.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TurmasComponent implements OnInit {
 
-  constructor() { }
+    btnSalvar: string = 'Salvar'
+    bgSalvar: string = 'salvar'
+    btnCancelar: string = 'Cancelar'
+    bgCancelar: string = 'cancelar'
+
+    turma: Turma = {
+        name: ''
+    }
+
+  constructor(
+      private turmasService: TurmasService,
+      private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  save(): void {
+      if(this.turma.name !== ''){
+        this.turmasService.create(this.turma).subscribe(
+            (result) => {
+                this.turma = result;
+                this.turmasService.showMessage('Nova turma inserida com sucesso')
+            })
+      } else {
+        this.turmasService.showMessage('Preencha os campos')
+      }  
+  }
+
+  cancel(): void {
+    this.router.navigate(['/secretaria'])
   }
 
 }
